05033c2a66f22042dbd3f610909dd751491c83fe
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-09 13:15:11 +0200

    feat: compent header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
 export default {
   name: "App",
+  components: {
+    AppHeader,
+  },
 };
 </script>
 
 <template>
-  <h1>ciao</h1>
+  <AppHeader />
 </template>
 
 <style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <h1>header</h1>
+</template>
+
+<style lang="scss" scoped>
+</style>
