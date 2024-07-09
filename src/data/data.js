bfe980878a0fe8893da180a8399d98a08660b79b
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-09 18:05:02 +0200

    fix: number of data card

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,7 @@
 
 export const data = {
     dataGame: [],
-    urlRichiesta: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+    urlRichiesta: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0',
     isLoad: false,
 
 
