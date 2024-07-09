f2d5e3068057a9392b558e10ac74cd8fc1f262ef
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-09 14:07:12 +0200

    feat: card in component cardgame

---
+++ src/components/CardGame.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "CardGame",
+};
+</script>
+
+<template>
+  <div class="contImg">
+    <img
+      src="https://images.ygoprodeck.com/images/cards/34541863.jpg"
+      alt="nome carta"
+    />
+
+    <h3>NOME DELLA CARTA</h3>
+    <span>tipo di carta</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "/src/style/partial/_variable.scss";
+.contImg {
+  text-align: center;
+  background-color: $colorPrincipal;
+  width: 8rem;
+  margin-bottom: 1rem;
+  h3 {
+    font-size: 0.8rem;
+    padding: 1rem;
+  }
+  span {
+    padding: 1rem;
+  }
+  img {
+    width: 100%;
+  }
+}
+</style>
--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,12 +1,57 @@
 <script>
+import CardGame from "./CardGame.vue";
 export default {
   name: "CharactersList",
+  components: {
+    CardGame,
+  },
 };
 </script>
 
 <template>
-  <h2>caracters</h2>
+  <div class="container">
+    <div class="contTitle">
+      <span>Found 39 card</span>
+    </div>
+
+    <div class="contCard">
+      <CardGame />
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+@import "../style/partial/variable.scss";
+.container {
+  width: 70%;
+  margin: auto;
+  background-color: white;
+  padding: 2rem;
+  .contTitle {
+    padding: 1rem 0.5rem;
+    background-color: black;
+    color: white;
+  }
+  .contCard {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    .contImg {
+      text-align: center;
+      background-color: $colorPrincipal;
+      width: 8rem;
+      margin-bottom: 1rem;
+      h3 {
+        font-size: 0.8rem;
+        padding: 1rem;
+      }
+      span {
+        padding: 1rem;
+      }
+      img {
+        width: 100%;
+      }
+    }
+  }
+}
 </style>
